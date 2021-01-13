4566022517327d92cc400d331fc4c16ac572c1ac
Eli Sheftolovich <elishafto@gmail.com>
2021-01-13 10:00:18 +0200

    all conectted

---
+++ src/components/ActorsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LiveSearchBox from './LiveSearchBox';
+
+class ActorsPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <LiveSearchBox/>
+            </div>
+        )
+    }
+
+}
+
+export default ActorsPage;
---
+++ src/components/LiveSearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LiveSearchBox.css';
+
+class LiveSearchBox extends React.Component{
+    render(){
+        return (
+            <div>
+               LiveSearchBox is here
+            </div>
+        )
+    }
+
+}
+
+export default LiveSearchBox;
